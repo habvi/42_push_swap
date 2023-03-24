fb3b264a38bda60ee28e162bf3e2a161999d521f
habvi <a2231250601h@gmail.com>
2023-03-24 14:51:10 +0100

    add: ft_dprintf

---
+++ libft/include/ft_dprintf.h
@@ -0,0 +1,6 @@
+#ifndef FT_DPRINTF_H
+# define FT_DPRINTF_H
+
+int	ft_dprintf(int fd, const char *format, ...);
+
+#endif
--- libft/include/libft.h
+++ libft/include/libft.h
@@ -6,11 +6,14 @@
 
 bool	ft_atoi_for_pushswap(const char *str, int *num);
 bool	ft_isdigit(int c);
+size_t  ft_max(const size_t x, const size_t y);
 size_t	ft_min(const size_t x, const size_t y);
 void	ft_putstr_fd(const char *s, int fd);
 char	**ft_split(char const *s, char c);
+char	*ft_strchr(const char *s, int int_c);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+int		ft_toupper(int c);
 
 #endif
