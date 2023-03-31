4c59133c007d7fdf9e9eb8d7b9af820cd851b6d5
habvi <a2231250601h@gmail.com>
2023-03-31 19:07:31 +0200

    add: is_valid_nums

--- src/nums.c
+++ src/nums.c
@@ -1,4 +1,5 @@
 #include <stdlib.h> // malloc
+#include "deque.h"
 #include "push_swap.h"
 #include "error.h"
 
@@ -17,11 +18,46 @@ t_nums	*nums_new(t_error *error_code)
 	return (nums);
 }
 
+static bool	is_same_number_in_stack_a(t_deque *node, t_deque *stack_a)
+{
+	while (stack_a)
+	{
+		if (stack_a == node)
+		{
+			stack_a = stack_a->next;
+			continue ;
+		}
+		if (node->num == stack_a->num)
+			return (true);
+		stack_a = stack_a->next;
+	}
+	return (false);
+}
+
+static bool	is_duplicate_numbers(t_nums *nums, t_error *error_code)
+{
+	t_deque	*stack_a;
+	t_deque	*node;
+
+	stack_a = nums->deque->next;
+	node = stack_a;
+	while (node)
+	{
+		if (is_same_number_in_stack_a(node, stack_a))
+		{
+			*error_code = ERROR_ARGS;
+			return (true);
+		}
+		node = node->next;
+	}
+	return (false);
+}
+
 bool	is_valid_nums(t_nums *nums, t_error *error_code)
 {
-	(void)nums;
-	(void)error_code;
-	// if not
-	// *error_code = ERROR_ARGS;
+	if (deque_is_empty(nums->deque))
+		return (true);
+	if (is_duplicate_numbers(nums, error_code))
+		return (false);
 	return (true);
 }
