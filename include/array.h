56f3b6c754269486bf8818e86ae1f476b5edc51a
habvi <a2231250601h@gmail.com>
2023-03-31 21:56:31 +0200

    fix: header protect

--- include/array.h
+++ include/array.h
@@ -1,11 +1,11 @@
-#ifndef ALLAY_H
-# define ALLAY_H
+#ifndef ARRAY_H
+# define ARRAY_H
 
 typedef struct s_nums	t_nums;
 typedef enum e_error	t_error;
 
 // array_copy.c
-int	*copy_to_array(t_nums *nums, t_error *error_code);
+int		*copy_to_array(t_nums *nums, t_error *error_code);
 
 // array_sub.c
 void	print_array(int *array, size_t size);
