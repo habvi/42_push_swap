ead175a3484530aa5381681431a49f5319d192b6
habvi <a2231250601h@gmail.com>
2023-04-19 14:31:02 +0200

    sep: array

--- include/array.h
+++ include/array.h
@@ -3,16 +3,17 @@
 
 # include <stddef.h> // size_t
 
+typedef struct s_deque	t_deque;
 typedef struct s_nums	t_nums;
 typedef enum e_error	t_error;
 
-// array_copy.c
-int		*copy_to_array(t_nums *nums, t_error *error_code);
+// copy.c
+int	    *copy_to_array(t_nums *nums, t_deque *allocated_ptrs, t_error *error_code);
 
-// array_print.c
+// print.c
 void	print_array(int *array, size_t size, char *array_name);
 
-// array_sort.c
+// sort.c
 void	sort_array(int *array, size_t size);
 
 #endif
---
+++ src/array/copy.c
@@ -0,0 +1,46 @@
+#include <stdlib.h> // malloc
+#include "deque.h"
+#include "error.h"
+#include "push_swap.h"
+
+static int	*allocate_array(size_t size, t_deque *allocated_ptrs, t_error *error_code)
+{
+	int	*array;
+
+	array = (int *)malloc(sizeof(int) * size);
+	if (array == NULL)
+	{
+		*error_code = ERROR_MALLOC;
+		return (NULL);
+	}
+	set_allocated_pointer(allocated_ptrs, POINTER, array, error_code);
+	return (array);
+}
+
+static void	copy_stack_a_to_array(t_nums *stack, int *array)
+{
+	t_deque	*stack_a;
+	size_t	size;
+	size_t	i;
+
+	stack_a = stack->deque->next;
+	size = stack->size;
+	i = 0;
+	while (i < size)
+	{
+		array[i] = stack_a->num;
+		stack_a = stack_a->next;
+		i++;
+	}
+}
+
+int	*copy_to_array(t_nums *stack, t_deque *allocated_ptrs, t_error *error_code)
+{
+	int	*array;
+
+	array = allocate_array(stack->size, allocated_ptrs, error_code);
+	if (*error_code)
+		return (NULL);
+	copy_stack_a_to_array(stack, array);
+	return (array);
+}
