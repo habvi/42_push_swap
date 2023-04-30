a98b28be66bd02c8165b25645423d4bcc7b0cd53
habvi <a2231250601h@gmail.com>
2023-04-30 13:36:17 +0200

    add: move min_op

--- src/solve_over_6/dfs.c
+++ src/solve_over_6/dfs.c
@@ -8,17 +8,60 @@
 
 #include <stdio.h> // to do: erase
 
+// static bool	is_dfs_end(t_block *block, t_data *data, t_error *error_code)
+// {
+// 	(void)block;
+// 	(void)data;
+// 	(void)error_code;
+// 	return (true);
+// }
+
 static t_nums	*last_nums_dfs(t_block *block, t_data *data, t_error *error_code)
 {
+	// unsigned int	i;
+
+	// if (is_dfs_end(block, data, error_code))
+	// 	return (data->now_op);
+	// i = 0;
+	// while (i < TOTAL_OPERATIONS)
+	// {
+	// 	if (!is_unnecessary_op())
+	// 	{
+	// 		i++;
+	// 		continue ;
+	// 	}
+	// 	run_operation(RUN);
+	// 	data->now_op = last_nums_dfs();
+	// 	run_operation(UNDO);
+	// 	i++;
+	// }
+	// return (min_op);
 	(void)block;
 	(void)error_code;
 	return (data->now_op);
 }
 
-static void	extend_min_op(t_nums *now_op, t_nums *min_op)
+// if run NONE, no error occurred
+static t_data	*move_min_op(t_data *data, t_deque *min_op, t_error *error_code)
+{
+	t_deque	*node;
+
+	if (deque_is_empty(min_op))
+		return (data);
+	node = min_op->next;
+	while (node)
+	{
+		run_operation(node->num, data, NONE, error_code);
+		node = node->next;
+	}
+	return (data);
+}
+
+static t_nums	*extend_min_op(t_nums *now_op, t_nums *min_op)
 {
 	deque_extend(now_op->deque, min_op->deque);
 	now_op->size += min_op->size;
+	return (now_op);
 }
 
 t_nums	*search_all_patterns(t_block *block, t_data *data, t_error *error_code)
@@ -39,7 +82,8 @@ t_nums	*search_all_patterns(t_block *block, t_data *data, t_error *error_code)
 	data->now_op = last_nums_dfs(block, data, error_code);
 	if (*error_code)
 		return (free_nums(min_op));
-	extend_min_op(data->now_op, min_op);
+	data = move_min_op(data, min_op->deque, error_code);
+	data->now_op = extend_min_op(data->now_op, min_op);
 	free(min_op);
 	return (data->now_op);
 }
