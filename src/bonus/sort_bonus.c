704ba4d16c8f8e8dc424c7f744b5ec2ef3b01d0d
habvi <a2231250601h@gmail.com>
2023-05-02 17:03:36 +0200

    add: move op, check sorted

--- src/bonus/sort_bonus.c
+++ src/bonus/sort_bonus.c
@@ -1,21 +1,37 @@
 #include "checker_bonus.h"
 #include "deque.h"
 #include "error.h"
+#include "free.h"
 #include "operations.h"
 #include "push_swap.h"
+#include "solve.h"
 
-void	move_op(t_nums *stack_a, t_nums *stack_b, t_operation op)
+// no error returns
+void	move_op(\
+		t_nums *stack_a, t_nums *stack_b, t_operation op, t_error *error_code)
 {
-	(void)stack_a;
-	(void)stack_b;
-	(void)op;
-	return ;
+	t_data	data;
+
+	data = (t_data){.stack_a = stack_a, .stack_b = stack_b};
+	run_operation(op, &data, NONE, error_code);
 }
 
 bool	is_stack_a_sorted_bonus(t_nums *stack_a, const size_t size_a)
 {
-	(void)stack_a;
-	(void)size_a;
+	t_deque	*node;
+	int		num;
+
+	node = stack_a->deque->next;
+	num = 1;
+	while (node)
+	{
+		if (node->num != num)
+			return (false);
+		node = node->next;
+		num++;
+	}
+	if ((size_t)num != size_a + 1)
+		return (false);
 	return (true);
 }
 
@@ -33,9 +49,11 @@ t_result	sort_and_judge(t_nums *stack_a, t_nums *ops, t_error *error_code)
 	while (node)
 	{
 		op = node->num;
-		move_op(stack_a, stack_b, op);
+		move_op(stack_a, stack_b, op, error_code);
 		node = node->next;
 	}
+	deque_print(stack_a->deque, "after stack_a");
+	free_nums(stack_b);
 	if (is_stack_a_sorted_bonus(stack_a, size_a))
 		return (OK);
 	return (KO);
