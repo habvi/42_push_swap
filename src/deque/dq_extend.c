b461aa85a62d244be1260980659abff95153ab61
habvi <a2231250601h@gmail.com>
2023-05-09 22:31:10 +0200

    replace(deque): add_back in extend

--- src/deque/dq_extend.c
+++ src/deque/dq_extend.c
@@ -4,7 +4,6 @@
 // unlink the sentinel and add_back from the next node
 void	deque_extend(t_deque *deque, t_deque *new_deque)
 {
-	t_deque	*tail;
 	t_deque	*new_head;
 	t_deque	*new_tail;
 
@@ -13,11 +12,9 @@ void	deque_extend(t_deque *deque, t_deque *new_deque)
 		free(new_deque);
 		return ;
 	}
-	tail = deque->prev;
 	new_head = new_deque->next;
 	new_tail = new_deque->prev;
-	deque_set_next(tail, new_head);
-	deque_set_prev(new_head, tail);
+	deque_add_back(deque, new_head);
 	deque_set_prev(deque, new_tail);
 	free(new_deque);
 }
